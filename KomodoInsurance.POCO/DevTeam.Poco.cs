7179796074ef2f97736658515fb51034e22d00aa
Andrew Broshears <ambroshears@gmail.com>
2021-12-10 20:24:04 -0600

    initial commit

---
+++ KomodoInsurance.POCO/DevTeam.Poco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoInsurance.POCO
+{
+    public class DevTeamPoco
+    {
+        public string DevMembers { get; set; }
+        public string DevTeamName { get; set; }
+        public int DevTeamId { get; set; }
+    }
+}
---
+++ KomodoInsurance.POCO/Developer.Poco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoInsurance.POCO
+{
+    public class DeveloperPoco
+    {
+        public string DeveloperName { get; set; }
+        public int DeveloperId { get; set; }
+        public bool Pluralsight { get; set; }
+    }
+}
